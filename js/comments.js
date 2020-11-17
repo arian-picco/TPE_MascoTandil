5b69d66918448070ea290de5216df6cce8f91503
arian-picco <apicco@sparkdigital.com>
2020-11-17 20:47:45 -0300

    js 1st apporach

---
+++ js/comments.js
@@ -0,0 +1,21 @@
+
+"use strict"
+
+document.addEventListener("DOMContentLoaded", iniciarPagina);
+
+function iniciarPagina(){
+
+//si el usuario esta loggeado debe aparecer un input en forma de text-box
+
+//tomar el id del contenedor y meterlo en una variable
+
+//Al cargar la pagina se generará un div que será child del contenedor 
+
+//dentro de ese dev habra un div por cada comentario y puntaje
+
+//también se genera una nueva entrada dentro del producto con el promedio del puntaje que recibió el articulo
+
+
+
+
+}
